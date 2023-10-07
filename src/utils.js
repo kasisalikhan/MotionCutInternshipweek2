d594fe7a0e77e41f046bf297cf68425afbae1e34
kasisalikhan <146641723+kasisalikhan@users.noreply.github.com>
2023-10-07 12:34:23 +0530

    Add files via upload

---
+++ src/utils.js
@@ -0,0 +1,48 @@
+//   ATTENTION!!!!!!!!!!!
+//   I SWITCHED TO PERMANENT DOMAIN
+//   DATA IS THE SAME JUST A DIFFERENT URL,
+//   DOES NOT AFFECT PROJECT FUNCTIONALITY
+
+const allProductsUrl = 'https://course-api.com/javascript-store-products'
+// temporary single product
+// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+const singleProductUrl =
+  'https://course-api.com/javascript-store-single-product'
+
+const getElement = (selection) => {
+  const element = document.querySelector(selection)
+  if (element) return element
+  throw new Error(`Please check "${selection}" selector, no such element exist`)
+}
+ 
+const formatPrice = (price) => {
+  let formattedPrice = Intl.NumberFormat('en-IN').format((price / 100).toFixed(2))
+  return price.toLocaleString('en-IN', {
+    maximumFractionDigits: 2,
+    style: 'currency',
+    currency: 'INR'
+});
+}
+
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item)
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item))
+  } else {
+    storageItem = []
+  }
+  return storageItem
+}
+
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item))
+}
+
+export {
+  allProductsUrl,
+  singleProductUrl,
+  getElement,
+  formatPrice,
+  getStorageItem,
+  setStorageItem,
+}
